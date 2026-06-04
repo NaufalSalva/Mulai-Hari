37ed74ec5b3a7466fd6c339a086819093b401a29
Dza-Dzh <dzakydzhn@student.upi.edu>
2026-06-04 15:52:46 +0700

    perbaiki error

--- apps/feature/habbits.cpp
+++ apps/feature/habbits.cpp
@@ -1,132 +1,131 @@
-#include <iostream>
-#include <string>
-#include "../utils/data_structure.cpp"
-using namespace std;
+// #include <iostream>
+// #include <string>
+// using namespace std;
 
-void tambahSubHabit(HabitNode* parent, string namaSubHabit)
-{
-    if(parent == nullptr)
-        return;
+// void tambahSubHabit(HabitNode* parent, string namaSubHabit)
+// {
+//     if(parent == nullptr)
+//         return;
 
-    HabitNode* baru = new HabitNode;
-    baru->nama = namaSubHabit;
-    baru->child = nullptr;
-    baru->sibling = nullptr;
+//     HabitNode* baru = new HabitNode;
+//     baru->nama = namaSubHabit;
+//     baru->child = nullptr;
+//     baru->sibling = nullptr;
 
-    if(parent->child == nullptr)
-    {
-        parent->child = baru;
-    }
-    else
-    {
-        HabitNode* temp = parent->child;
+//     if(parent->child == nullptr)
+//     {
+//         parent->child = baru;
+//     }
+//     else
+//     {
+//         HabitNode* temp = parent->child;
 
-        while(temp->sibling != nullptr)
-        {
-            temp = temp->sibling;
-        }
+//         while(temp->sibling != nullptr)
+//         {
+//             temp = temp->sibling;
+//         }
 
-        temp->sibling = baru;
-    }
-}
+//         temp->sibling = baru;
+//     }
+// }
 
-HabitNode* cariHobi(HabitNode* root, string namaHobi)
-{
-    if(root == nullptr)
-        return nullptr;
+// HabitNode* cariHobi(HabitNode* root, string namaHobi)
+// {
+//     if(root == nullptr)
+//         return nullptr;
 
-    HabitNode* temp = root->child;
+//     HabitNode* temp = root->child;
 
-    while(temp != nullptr)
-    {
-        if(temp->nama == namaHobi)
-            return temp;
+//     while(temp != nullptr)
+//     {
+//         if(temp->nama == namaHobi)
+//             return temp;
 
-        temp = temp->sibling;
-    }
+//         temp = temp->sibling;
+//     }
 
-    return nullptr;
-}
+//     return nullptr;
+// }
 
-void generateSubHabitOtomatis(HabitNode* root)
-{
-    if(root == nullptr)
-        return;
+// void generateSubHabitOtomatis(HabitNode* root)
+// {
+//     if(root == nullptr)
+//         return;
 
-    HabitNode* currentHobi = root->child;
+//     HabitNode* currentHobi = root->child;
 
-    while(currentHobi != nullptr)
-    {
-        if(currentHobi->nama == "Olahraga")
-        {
-            tambahSubHabit(currentHobi,"Lari Pagi 15 Menit");
-            tambahSubHabit(currentHobi,"Stretching & Pemanasan");
-        }
-        else if(currentHobi->nama == "Coding")
-        {
-            tambahSubHabit(currentHobi,"Eksplorasi Sintaks C++");
-            tambahSubHabit(currentHobi,"Debugging Error");
-            tambahSubHabit(currentHobi,"Riset Algoritma");
-        }
-        else if(currentHobi->nama == "Membaca")
-        {
-            tambahSubHabit(currentHobi,"Baca 10 Halaman");
-            tambahSubHabit(currentHobi,"Review Bab Terakhir");
-        }
-        else if(currentHobi->nama == "Masak")
-        {
-            tambahSubHabit(currentHobi,"Siapkan Bahan");
-            tambahSubHabit(currentHobi,"Eksperimen Resep Baru");
-        }
-        else
-        {
-            tambahSubHabit(currentHobi,"Persiapan Aktivitas");
-            tambahSubHabit(currentHobi,"Evaluasi Sesi");
-        }
+//     while(currentHobi != nullptr)
+//     {
+//         if(currentHobi->nama == "Olahraga")
+//         {
+//             tambahSubHabit(currentHobi,"Lari Pagi 15 Menit");
+//             tambahSubHabit(currentHobi,"Stretching & Pemanasan");
+//         }
+//         else if(currentHobi->nama == "Coding")
+//         {
+//             tambahSubHabit(currentHobi,"Eksplorasi Sintaks C++");
+//             tambahSubHabit(currentHobi,"Debugging Error");
+//             tambahSubHabit(currentHobi,"Riset Algoritma");
+//         }
+//         else if(currentHobi->nama == "Membaca")
+//         {
+//             tambahSubHabit(currentHobi,"Baca 10 Halaman");
+//             tambahSubHabit(currentHobi,"Review Bab Terakhir");
+//         }
+//         else if(currentHobi->nama == "Masak")
+//         {
+//             tambahSubHabit(currentHobi,"Siapkan Bahan");
+//             tambahSubHabit(currentHobi,"Eksperimen Resep Baru");
+//         }
+//         else
+//         {
+//             tambahSubHabit(currentHobi,"Persiapan Aktivitas");
+//             tambahSubHabit(currentHobi,"Evaluasi Sesi");
+//         }
 
-        currentHobi = currentHobi->sibling;
-    }
-}
+//         currentHobi = currentHobi->sibling;
+//     }
+// }
 
-void cetakTreeLCRS(HabitNode* node,
-                   string prefix = "",
-                   bool isLast = true,
-                   bool isRoot = true)
-{
-    if(node == nullptr)
-        return;
+// void cetakTreeLCRS(HabitNode* node,
+//                    string prefix = "",
+//                    bool isLast = true,
+//                    bool isRoot = true)
+// {
+//     if(node == nullptr)
+//         return;
 
-    if(isRoot)
-    {
-        cout << "[Root] " << node->nama << endl;
-    }
-    else
-    {
-        cout << prefix;
-        cout << (isLast ? "└── " : "├── ");
-        cout << node->nama << endl;
-    }
+//     if(isRoot)
+//     {
+//         cout << "[Root] " << node->nama << endl;
+//     }
+//     else
+//     {
+//         cout << prefix;
+//         cout << (isLast ? "└── " : "├── ");
+//         cout << node->nama << endl;
+//     }
 
-    string nextPrefix =
-        prefix +
-        (isLast && !isRoot
-        ? "    "
-        : (isRoot ? "" : "│   "));
+//     string nextPrefix =
+//         prefix +
+//         (isLast && !isRoot
+//         ? "    "
+//         : (isRoot ? "" : "│   "));
 
-    HabitNode* child = node->child;
+//     HabitNode* child = node->child;
 
-    while(child != nullptr)
-    {
-        bool lastSibling =
-            (child->sibling == nullptr);
+//     while(child != nullptr)
+//     {
+//         bool lastSibling =
+//             (child->sibling == nullptr);
 
-        cetakTreeLCRS(
-            child,
-            nextPrefix,
-            lastSibling,
-            false
-        );
+//         cetakTreeLCRS(
+//             child,
+//             nextPrefix,
+//             lastSibling,
+//             false
+//         );
 
-        child = child->sibling;
-    }
-}
+//         child = child->sibling;
+//     }
+// }
