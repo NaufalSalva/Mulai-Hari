dd57a8e635768568721383cf76e1ca86d9fde0bd
Vowed69 <mhmmdarfa@student.upi.edu>
2026-06-05 00:01:17 +0700

    Form All done

--- apps/feature/forms/form_membaca.cpp
+++ apps/feature/forms/form_membaca.cpp
@@ -4,17 +4,18 @@
 #include <cstdlib>
 using namespace std;
 
-struct FormGaming {
-    string namaGame;
-    string genreGame;
-    string tujuanMain;
+struct FormMembaca {
+    string judulBacaan;
+    string genreBacaan;
+    string mediaBacaan;
+    string tujuanMembaca;
     int durasiMenit;
     string catatan;
     string waktuAktivitas;
 };
 
-bool validasiNamaGame(const string& nama) {
-    if (nama.empty() || nama.length() < 2) {
+bool validasiJudulBacaan(const string& judul) {
+    if (judul.empty() || judul.length() < 2) {
         return false;
     }
     return true;
@@ -45,28 +46,28 @@ string pilihanWaktu(int pilihan) {
 }
 
 
-FormGaming inputFormGaming() {
-    FormGaming form;
+FormMembaca inputFormMembaca() {
+    FormMembaca form;
     int pilihan;
     
     system("cls");
     cout << "\n";
     cout << "=============================================\n";
-    cout << "            FORM AKTIVITAS GAMING        \n";
+    cout << "            FORM AKTIVITAS MEMBACA        \n";
     cout << "=============================================\n\n";
     
 
-    cout << "============= 1. NAMA GAME =============\n";
-    cout << "Nama Game: ";
-    getline(cin, form.namaGame);
+    cout << "============= 1. JUDUL BACAAN =============\n";
+    cout << "Judul Bacaan: ";
+    getline(cin, form.judulBacaan);
     
-    while (!validasiNamaGame(form.namaGame)) {
-        cout << "Nama game tidak valid! Masukkan lagi: ";
-        getline(cin, form.namaGame);
+    while (!validasiJudulBacaan(form.judulBacaan)) {
+        cout << "Judul bacaan tidak valid! Masukkan lagi: ";
+        getline(cin, form.judulBacaan);
     }
     
 
-    cout << "============= 2. DURASI MENULIS =============\n";
+    cout << "============= 2. DURASI MEMBACA =============\n";
     cout << "Masukkan durasi (dalam menit): ";
     cin >> form.durasiMenit;
     
@@ -76,60 +77,79 @@ FormGaming inputFormGaming() {
     }
     cin.ignore();
 
-    cout << "\n============= 3. GENRE GAME =============\n";
+    cout << "\n============= 3. GENRE BACAAN =============\n";
     cout << "1. Action\n";
-    cout << "2. RPG\n";
-    cout << "3. FPS\n";
-    cout << "4. MOBA\n";
-    cout << "5. Strategy\n";
-    cout << "6. Simulation\n";
-    cout << "7. Sports\n";
-    cout << "8. Racing\n";
-    cout << "9. Lainnya\n";
-    cout << "Pilih genre (1-9): ";
+    cout << "2. Fantasi\n";
+    cout << "3. Komedi\n";
+    cout << "4. Misteri\n";
+    cout << "5. Horror\n";
+    cout << "6. Thriller\n";
+    cout << "7. Lainnya\n";
+    cout << "Pilih genre (1-7): ";
     cin >> pilihan;
     cin.ignore();
 
     switch (pilihan) {
-        case 1: form.genreGame = "Action"; break;
-        case 2: form.genreGame = "RPG"; break;
-        case 3: form.genreGame = "FPS"; break;
-        case 4: form.genreGame = "MOBA"; break;
-        case 5: form.genreGame = "Strategy"; break;
-        case 6: form.genreGame = "Simulation"; break;
-        case 7: form.genreGame = "Sports"; break;
-        case 8: form.genreGame = "Racing"; break;
-        default: form.genreGame = "Lainnya"; break;
+        case 1: form.genreBacaan = "Action"; break;
+        case 2: form.genreBacaan = "Fantasi"; break;
+        case 3: form.genreBacaan = "Komedi"; break;
+        case 4: form.genreBacaan = "Misteri"; break;
+        case 5: form.genreBacaan = "Horror"; break;
+        case 6: form.genreBacaan = "Thriller"; break;
+        case 7: form.genreBacaan = "Lainnya"; break;
+        default: form.genreBacaan = "Lainnya"; break;
     }
 
-     cout << "\n============= 4. TUJUAN MAIN =============\n";
-    cout << "1. Hiburan\n";
-    cout << "2. Menghilangkan Stres\n";
-    cout << "3. Chill With Bros\n";
-    cout << "4. Latihan\n";
-    cout << "5. Mencari Achievement\n";
+    cout << "\n============= 4. MEDIA BACA =============\n";
+    cout << "1. Buku Fisik\n";
+    cout << "2. E-Book\n";
+    cout << "3. Jurnal Online\n";
+    cout << "4. Website\n";
+    cout << "5. PDF\n";
     cout << "6. Lainnya\n";
-    cout << "Pilih genre (1-6): ";
+    cout << "Pilih media (1-6): ";
     cin >> pilihan;
     cin.ignore();
 
     switch (pilihan) {
-        case 1: form.tujuanMain = "Hiburan"; break;
-        case 2: form.tujuanMain = "Menghilangkan Stres"; break;
-        case 3: form.tujuanMain = "Chill With Bros"; break;
-        case 4: form.tujuanMain = "Latihan"; break;
-        case 5: form.tujuanMain = "Mencari Achievement"; break;
-        case 6: form.tujuanMain = "Lainnya"; break;
+        case 1: form.mediaBacaan = "Buku Fisik"; break;
+        case 2: form.mediaBacaan = "E-Book"; break;
+        case 3: form.mediaBacaan = "Jurnal Online"; break;
+        case 4: form.mediaBacaan = "Website"; break;
+        case 5: form.mediaBacaan = "PDF"; break;
+        case 6: form.mediaBacaan = "Lainnya"; break;
+        default: form.mediaBacaan = "Lainnya"; break;
     }
 
-    cout << "\n============= 5. WAKTU AKTIVITAS =============\n";
+     cout << "\n============= 5. TUJUAN MEMBACA =============\n";
+    cout << "1. Belajar\n";
+    cout << "2. Hiburan\n";
+    cout << "3. Riset\n";
+    cout << "4. Mencari Inspirasi\n";
+    cout << "5. Menambah Wawasan\n";
+    cout << "6. Lainnya\n";
+    cout << "Pilih tujuan (1-6): ";
+    cin >> pilihan;
+    cin.ignore();
+
+    switch (pilihan) {
+        case 1: form.tujuanMembaca = "Belajar"; break;
+        case 2: form.tujuanMembaca = "Hiburan"; break;
+        case 3: form.tujuanMembaca = "Riset"; break;
+        case 4: form.tujuanMembaca = "Mencari Inspirasi"; break;
+        case 5: form.tujuanMembaca = "Menambah Wawasan"; break;
+        case 6: form.tujuanMembaca = "Lainnya"; break;
+        default: form.tujuanMembaca = "Lainnya"; break;
+    }
+
+    cout << "\n============= 6. WAKTU AKTIVITAS =============\n";
     tampilkanMenuWaktu();
     cout << "Pilih waktu (1-4): ";
     cin >> pilihan;
     form.waktuAktivitas = pilihanWaktu(pilihan);
     cin.ignore();
 
-    cout << "\n============= 6. CATATAN TAMBAHAN =============\n";
+    cout << "\n============= 7. CATATAN TAMBAHAN =============\n";
     cout << "Catatan: ";
     getline(cin, form.catatan);
     
@@ -137,15 +157,17 @@ FormGaming inputFormGaming() {
 }
 
 
-string standardisasiDetailGaming(const FormGaming& form)
+string standardisasiDetailMembaca(const FormMembaca& form)
 {
     string detail = "";
 
-    detail += form.namaGame;
+    detail += form.judulBacaan;
     detail += " | ";
-    detail += form.genreGame;
+    detail += form.genreBacaan;
     detail += " | ";
-    detail += form.tujuanMain;
+    detail += form.tujuanMembaca;
+    detail += " | ";
+    detail += form.mediaBacaan;
     detail += " | ";
     detail += to_string(form.durasiMenit) + " menit";
     detail += " | Waktu: " + form.waktuAktivitas;
@@ -159,18 +181,19 @@ string standardisasiDetailGaming(const FormGaming& form)
 }
 
 
-void tampilkanPreviewGaming(const FormGaming& form) {
+void tampilkanPreviewMembaca(const FormMembaca& form) {
     system("cls");
     cout << "\n";
     cout << "=============================================\n";
-    cout << "           PREVIEW AKTIVITAS GAMING       \n";
+    cout << "           PREVIEW AKTIVITAS MEMBACA       \n";
     cout << "=============================================\n\n";
     
     cout << "Detail Aktivitas:\n";
     cout << "=============================================\n";
-    cout << "Nama Game : " << form.namaGame << "\n";
-    cout << "Genre Game     : " << form.genreGame << "\n";
-    cout << "Tujuan Main    : " << form.tujuanMain << "\n";
+    cout << "Judul Bacaan : " << form.judulBacaan << "\n";
+    cout << "Genre Bacaan     : " << form.genreBacaan << "\n";
+    cout << "Tujuan Membaca    : " << form.tujuanMembaca << "\n";
+    cout << "Media Bacaan         : " << form.mediaBacaan << "\n";
     cout << "Durasi         : " << form.durasiMenit << " menit\n";
     cout << "Waktu Aktivitas: " << form.waktuAktivitas << "\n";
 
@@ -180,14 +203,14 @@ void tampilkanPreviewGaming(const FormGaming& form) {
     
     cout << "\n=============================================\n";
     cout << "String Detail Terstandarisasi:\n";
-    cout << standardisasiDetailGaming(form) << "\n";
+    cout << standardisasiDetailMembaca(form) << "\n";
     cout << "=============================================\n";
     
     cout << "\nTekan ENTER untuk kembali...";
     cin.ignore();
 }
 
-void formGaming() {
-    FormGaming form = inputFormGaming();
-    tampilkanPreviewGaming (form);
+void formMembaca() {
+    FormMembaca form = inputFormMembaca();
+    tampilkanPreviewMembaca(form);
 }
